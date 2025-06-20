af48521daa461a0025c93d6a9bb1627629de8e8a
Abdul Basith <159468770+basthep@users.noreply.github.com>
2025-06-20 18:59:18 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -11,11 +11,12 @@ const app = express()
 
 app.use(cors())
 
-app.post("/api/clerk", express.raw({ type: "*/*" }), clerkWebhooks);
-
 // Middleware
 app.use(express.json())
 app.use(clerkMiddleware())
+
+// API to listen Clerk Webhooks
+app.use("/api/clerk", clerkWebhooks);
 
 
 app.get('/', (req,res)=> res.send("API is working"))
